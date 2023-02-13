49ee8d8b38273d498cb671d310d29b6b7fb51695
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-02-14 01:19:17 +0300

    Task2

---
+++ Task2.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+
+
+
+
+int MaxNumber (int a,int b,int c)
+{
+    int max = 0;
+    if (a > b & a > c)
+    {
+        max = a;
+        
+    }else if (b > c & b > a)
+    {
+        max = b;
+
+    }else if (c > b & c > a)
+    {
+        max = c;
+    }
+    return max;
+
+
+
+}
+
+Console.WriteLine(MaxNumber(4, 22, 111));
